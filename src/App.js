7aec4b898929fb4625bea998b15a8e5dd524a1cc
ADIsaacNascimento <104455835+ADIsaacNascimento@users.noreply.github.com>
2022-10-09 00:39:48 -0300

    inital commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+export const App = () => {
+  const showpNow = "Shop Now";
+  const categories = [
+    { id: 1, title: "Hats" },
+    { id: 2, title: "Jackets" },
+    { id: 3, title: "Sneakers" },
+    { id: 4, title: "Womens" },
+    { id: 5, title: "Mens" },
+  ];
+
+  return (
+    <div className="categories-container">
+      {categories.map((item) => (
+        <div key={item.id} className="category-container">
+          <div className="background-image" />
+          <div className="category-body-container">
+            <h2>{item.title}</h2>
+            <p>{showpNow}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
