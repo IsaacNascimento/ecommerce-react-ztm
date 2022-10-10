b924e63de5ea9e7543f0901ad79c1a5e8267380b
Isaac Nascimento <isaacs.nascimento17@gamil.com>
2022-10-10 14:22:50 -0300

    Delete comment

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./categories.styles.scss";
 import { Directory } from "./components/directory/directory.component";
 
-// New Comment
 export const App = () => {
   return (
     <div className="categories-container">
