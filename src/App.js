fd7bf3cb2971f27d8726c3c45e46d8a6af6d9075
ADIsaacNascimento <104455835+ADIsaacNascimento@users.noreply.github.com>
2022-10-09 22:23:39 -0300

    Directory, category item component. Google Font, saas

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
 import React from "react";
 import './categories.styles.scss';
+import { Directory } from "./components/directory/directory.component";
 
 export const App = () => {
-  const showpNow = "Shop Now";
-  const categories = [
-    { id: 1, title: "Hats" },
-    { id: 2, title: "Jackets" },
-    { id: 3, title: "Sneakers" },
-    { id: 4, title: "Womens" },
-    { id: 5, title: "Mens" },
-  ];
-
   return (
     <div className="categories-container">
-      {categories.map((item) => (
-        <div key={item.id} className="category-container">
-          <div className="background-image" />
-          <div className="category-body-container">
-            <h2>{item.title}</h2>
-            <p>{showpNow}</p>
-          </div>
-        </div>
-      ))}
+      <Directory/>
     </div>
   );
 };
