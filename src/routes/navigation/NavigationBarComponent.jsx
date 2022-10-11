25cbc94ab27d75c088332aaa50ee9636ce71a814
Isaac Nascimento <isaacs.nascimento17@gamil.com>
2022-10-11 17:49:47 -0300

    Logo, styles, navigation component and etc

--- src/routes/navigation/NavigationBarComponent.jsx
+++ src/routes/navigation/NavigationBarComponent.jsx
@@ -1,12 +1,14 @@
 import React, { Fragment } from 'react'
 import { Link, Outlet } from 'react-router-dom'
+import './navigation.styles.scss';
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg'; 
 
 export const NavigationBar = () => {
   return (
     <Fragment>
       <div className='navigation'>
         <Link className='logo-container' to={'/'}>
-            <div>Logo</div>
+            <CrwnLogo className='logo'/>
         </Link>
         <div className='nav-links-container'>
           <Link className='nav-link' to={'/shop'}>
