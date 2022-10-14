2e0593ce71a0e87e8599ab3a0cdbca3db089f5e4
Isaac Nascimento <isaacs.nascimento17@gamil.com>
2022-10-14 14:19:51 -0300

    Creating user by email/password. 'Odd way to do that' - Dirty code

--- src/components/sign-up-form/signUpFormComponent.jsx
+++ src/components/sign-up-form/signUpFormComponent.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { createAuthUserWithEmailAndPassword } from "../../utils/firebase/firebaseUtil";
 
 const defaultFormFields = {
   displayName: "",
@@ -11,25 +12,48 @@ export const SignUp = () => {
   const [formFields, setFormFields] = useState(defaultFormFields);
   const { displayName, email, password, confirmPassword } = formFields;
 
-  // console.log(formFields);
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (password !== confirmPassword) {
+      alert("Passwords do not match");
+    } else {
+      const fields = event.nativeEvent.srcElement;
+      const fieldsLenght = event.nativeEvent.srcElement.length;
+      const fieldsValues = [];
+      for (let i = 0; i < fieldsLenght; i++) {
+        fieldsValues.push(fields[i].value);
+      }
+
+      const data = {
+        email: fieldsValues[1],
+        password: fieldsValues[3],
+      };
+
+      const value = { ...data };
+      if (!value.password || !value.email) return;
+      return await createUser(value.email, value.password);
+    }
+  };
+
+  const createUser = async (email, password) => {
+    return await createAuthUserWithEmailAndPassword(email, password);
+  };
 
   const handleChange = (event) => {
-    // console.log(event.target);
     const { name, value } = event.target;
-    // console.log(name);
-    // console.log(value);
-    setFormFields({...formFields, [name]: value});
+    setFormFields({ ...formFields, [name]: value });
   };
 
   return (
     <div>
       <h1>Sign up with your email and password</h1>
-      <form onSubmit={() => {}}>
+      <form onSubmit={handleSubmit}>
         <label>Display Name</label>
         <input
           type="text"
           required
-          onChange={handleChange}
+          onChange={(e) => handleChange(e)}
           name="displayName"
           value={displayName}
         />
